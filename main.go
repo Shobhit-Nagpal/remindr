2dbedf9abe7fd5e14365bac4ce31fbeafc06f470
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-01-23 22:09:56 +0530

    basic implementation

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello from Remindr")
+
+	ticker := time.NewTicker(5 * time.Second)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				notifyCmd := exec.Command("notify-send", "get up, bhai. kya ho raha hai")
+				if err := notifyCmd.Run(); err != nil {
+					log.Fatal(err)
+				}
+
+			}
+		}
+	}()
+
+  time.Sleep(30 * time.Second)
 }
